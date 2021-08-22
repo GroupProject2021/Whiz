a155bd867e005203aedc83425e61ea2012402994
Dhanushka sandakelum <dhanushkasandakelum711@gmail.com>
2021-08-22 21:36:40 +0530

    created the folder structure

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -1,6 +1,14 @@
 <?php require APPROOT.'/views/inc/header.php'; ?>
+<?php require APPROOT.'/views/inc/navbar.php'; ?>
 <h1><?php echo $data['title']; ?></h1>
-<ul>
+<form action="" method="post">
+    <p>Name: <input type="text" name="name"></p>
+    <p>Email: <input type="text" name="email"></p>
+    <p>Password: <input type="password" name="pwd"></p>
+    <p>Confirm password: <input type="password" name="c_pwd"></p>
+    <p><input type="submit" value="submit"></p>
+</form>
+    <ul>
         <?php foreach($data['users'] as $user) :?>
         <li><?php echo $user->user_name; ?></li>
         <?php endforeach;?>
