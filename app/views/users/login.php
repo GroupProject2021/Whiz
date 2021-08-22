0eb1411cc2937a27c706eff857b57e69395bd6f9
Risini Piyathma <risinipiyathma1998@gmail.com>
2021-08-22 21:12:39 +0530

    find some DB issue

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -1,3 +1,8 @@
 <?php require APPROOT.'/views/inc/header.php'; ?>
 <h1><?php echo $data['title']; ?></h1>
+<ul>
+        <?php foreach($data['users'] as $user) :?>
+        <li><?php echo $user->user_name; ?></li>
+        <?php endforeach;?>
+    </ul>
 <?php require APPROOT.'/views/inc/footer.php'; ?>
