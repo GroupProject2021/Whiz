dea22f91ea174801cad13c51b41fb125182ab7e9
Dhanushka sandakelum <dhanushkasandakelum711@gmail.com>
2021-08-22 15:06:18 +0530

    clean up the Pages controller

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,11 +1,11 @@
 <?php
     class Pages {
         public function __construct() {
-            echo 'Pages loaded';
+            
         }
 
         public function index() {
-            
+
         }
 
         public function about($id) {
