889fa34ea5822647c3dcf8b5904e1d4b7b2ddc6b
GroupProject2021 <86022597+GroupProject2021@users.noreply.github.com>
2021-08-31 09:53:33 +0530

    Update login.php

--- app/views/admin/login.php
+++ app/views/admin/login.php
@@ -1,17 +1,34 @@
-<!DOCTYPE html>
-<html>
+<html lang="en">
+    <head>
+        <title><?php echo SITENAME; ?></title>
+        <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
+        <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/users/login_style.css">
+    </head>
     <body>
-        <form method="post" action="<?php echo URL; ?>Admin/signup">
-            First name:<br>
-            <input type="text" name="user_name" >
-            <br>
-            Email:<br>
-            <input type="text" name="email_id">
-            <br><br>
-            Password:<br>
-            <input type="password" name="password">
-            <br><br>
-            <input type="submit" value="Submit">
-        </form> 
+        <!-- NAVIGATION BAR -->
+        <?php require APPROOT.'/views/inc/mentor_login_navbar.php'?>
+        <!-- LOGIN FORM -->
+        <form action="<?php echo URLROOT; ?>/mentors/login" method="post">
+            <div class="container">
+                <h1>Mentor Login</h1>
+                <p>Please enter your credentials</p>
+                <hr>
+                <!-- flash message -->              
+                <?php flash('register_success'); ?>
+
+                <label for="email"><b>Email</b></label>
+                <input type="text" placeholder="Enter email" name="email" id="email" value="<?php echo $data['email']; ?>">
+                <span class="invalid"><?php echo $data['email_err']; ?></span><br>
+
+                <label for="password"><b>Password</b></label>
+                <input type="text" placeholder="Enter password" name="password" id="password" value="<?php echo $data['password']; ?>">
+                <span class="invalid"><?php echo $data['password_err']; ?></span><br>
+                <hr>
+                <button type="submit" class="loginBtn">Login</button>
+            </div>
+            <div class="container register">
+                <p>Don't have an account? <a href="<?php echo URLROOT; ?>/mentors/register">Register</a></p>
+            </div>
+        </form>
     </body>
 </html>
