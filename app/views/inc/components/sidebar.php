e7cc87edfc02811db9630b62a715b7de7a87c479
Dhanushka sandakelum <dhanushkasandakelum711@gmail.com>
2021-08-31 08:58:01 +0530

    beginner dashboard created

---
+++ app/views/inc/components/sidebar.php
@@ -0,0 +1,70 @@
+<input type="checkbox" name="" id="sidebar-toggle">
+
+<div class="sidebar">
+    <!-- sidebar header -->
+    <div class="sidebar-brand">
+        <div class="brand-flex">
+            <img src="<?php echo URLROOT; ?>/imgs/sidebar/logo.jpg" width="50px" height="40px" alt="logo">
+
+            <div class="brand-icons">
+                <img src="<?php echo URLROOT; ?>/imgs/sidebar/icon.png">
+                <img src="<?php echo URLROOT; ?>/imgs/sidebar/icon.png">
+            </div>
+        </div>
+    </div>
+
+    <!-- sidebar main -->
+    <div class="sidebar-main">
+        <div class="sidebar-user">
+            <img src="<?php echo URLROOT; ?>/imgs/sidebar/2.jpg" alt="prof_pic">
+            <div>
+                <h3>
+                    Dhanushka
+                </h3>
+                <span>
+                    dhanushka@gmail.com
+                </span>
+            </div>
+        </div>
+
+        <div class="sidebar-menu">
+            <div class="menu-head">
+                <span>Dashboard</span>
+            </div>
+            <ul>
+                <li>
+                    <a href="">
+                        <span class=""></span>
+                        Finance
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span class=""></span>
+                        Analytics
+                    </a>
+                </li>
+            </ul>
+
+            <div class="menu-head">
+                <span>Applications</span>
+            </div>
+            <ul>
+                <li>
+                    <a href="">
+                        <span class=""></span>
+                        Something
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span class=""></span>
+                        Anything
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<label for="sidebar-toggle" class="body-label"></label>
