60cfb3a760b2a1ca5a6905bb4c62ad0946125098
GroupProject2021 <86022597+GroupProject2021@users.noreply.github.com>
2021-09-02 14:15:44 +0530

    Update login.php

--- app/views/admin/login.php
+++ app/views/admin/login.php
@@ -1,31 +1,31 @@
 <html lang="en">
     <head>
         <title><?php echo SITENAME; ?></title>
+        <!-- Styles -->
         <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
-        <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/users/login_style.css">
     </head>
     <body>
-        <!-- NAVIGATION BAR -->
-        <?php require APPROOT.'/views/inc/admin_login_navbar.php'?>
+        <!-- TOP NAVIGATION BAR -->
+        <?php require APPROOT.'/views/inc/components/topnav.php'?>
+        
         <!-- LOGIN FORM -->
-        <form action="<?php echo URLROOT; ?>/admin/login" method="post">
-            <div class="form-container">
+        <div class="form-container">
+            <form action="<?php echo URLROOT; ?>/admins/login" method="post">
                 <h1>Admin Login</h1>
                 <p>Please enter your credentials</p>
-                <hr>
-                <label for="email"><b>Email</b></label>
+                <hr  class="form-hr">
+                <label for="email"><p class="form-bold">Email</p></label>
                 <input type="text" placeholder="Enter email" name="email" id="email" value="<?php echo $data['email']; ?>">
-                <span class="invalid"><?php echo $data['email_err']; ?></span><br>
-
-                <label for="password"><b>Password</b></label>
-                <input type="text" placeholder="Enter password" name="password" id="password" value="<?php echo $data['password']; ?>">
-                <span class="invalid"><?php echo $data['password_err']; ?></span><br>
-                <hr>
-                <button type="submit" class="loginBtn">Login</button>
-            </div>
-            <div class="form-container register">
-                <p>Don't have an account? <a href="<?php echo URLROOT; ?>/admin/register">Register</a></p>
-            </div>
-        </form>
+                <span class="form-invalid"><?php echo $data['email_err']; ?></span><br>
+                <label for="password"><p class="form-bold">Password</p></label>
+                <input type="text" placeholder="Enter email" name="password" id="password" value="<?php echo $data['password']; ?>">
+                <span class="form-invalid"><?php echo $data['password_err']; ?></span><br>
+                <hr  class="form-hr">
+                <button type="submit" class="form-submit">Login</button>
+            </form>
+        </div>
+        <div class="form-container content">
+            <p>Don't have an account? <a class="form-link" href="<?php echo URLROOT; ?>/admins/register">Register</a></p>
+        </div>
     </body>
 </html>
