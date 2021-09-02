8092932227eb8bd4b67500658ae4852d77171331
GroupProject2021 <86022597+GroupProject2021@users.noreply.github.com>
2021-09-02 13:17:36 +0530

    Update Admin.php

---
+++ app/models/Admin.php
@@ -0,0 +1,43 @@
+<?php
+    class Admin {
+        private $db;
+
+        public function __construct() {
+            $this->db = new Database;
+        }
+        // Login user
+        public function login($email, $password) {
+            $this->db->query('SELECT * FROM Admin WHERE email = :email');
+            // bind values
+            $this->db->bind(':email', $email);
+
+            $row = $this->db->single();
+
+            $hashed_password = $row->password;
+            if(password_verify($password, $hashed_password)) {
+                return $row;
+            }
+            else {
+                return false;
+            }
+        }
+
+
+        // Find user by email
+        public function findUserByEmail($email) {
+            $this->db->query('SELECT * FROM Admin WHERE email = :email'); // this is a prepared statement
+            // bind value
+            $this->db->bind(":email", $email);
+
+            $row = $this->db->single();
+
+            // Check row - return true if email exists. Because then rowCount is not 0
+            if($this->db->rowCount() > 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+?>
