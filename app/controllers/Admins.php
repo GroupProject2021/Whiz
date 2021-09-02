7c4b1fe9464a710e9d66eb64c7c197d5b130a6de
GroupProject2021 <86022597+GroupProject2021@users.noreply.github.com>
2021-09-02 09:40:45 +0530

    Update Admins.php

---
+++ app/controllers/Admins.php
@@ -0,0 +1,103 @@
+<?php
+    class Students extends Controller {
+        public function __construct() {
+            $this->studentModel = $this->model('Student');
+        }
+public function login() {
+            // Check for POST
+            if($_SERVER['REQUEST_METHOD'] == 'POST') {
+                // Process form
+
+                // Sanitize POST data
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+                // Init data
+                $data = [
+                    'email' => trim($_POST['email']),
+                    'password' => trim($_POST['password']),
+                    'email_err' => '',
+                    'password_err' => ''
+                ];
+
+                // Validate email
+                if(empty($data['email'])) {
+                    $data['email_err'] = 'Please enter email';
+                }
+
+                // Validate password
+                if(empty($data['password'])) {
+                    $data['password_err'] = 'Please enter password';
+                }
+
+                // Check for user/email
+                if($this->studentModel->findUserByEmail($data['email'])) {
+                    // User found
+                }
+                else {
+                    //user not found
+                    $data['email_err'] = 'No user found';
+                }
+
+                // Make sure errors are empty
+                if(empty($data['email_err']) && empty($data['password_err'])) {
+                    // Validated
+                    // Check and set logged in user
+                    $loggedInUser = $this->studentModel->login($data['email'], $data['password']);
+
+                    if($loggedInUser) {
+                        // Create session
+                        $this->createUserSession($loggedInUser);
+                    }
+                    else {
+                        $data['password_err'] = 'Password incorrect';
+
+                        $this->view('students/student_login', $data);
+                    }
+                }
+                else {
+                    // Load view with errors
+                    $this->view('students/student_login', $data);
+                }
+            }
+            else {
+                // Init data
+                $data = [
+                    'email' => '',
+                    'password' => '',
+                    'email_err' => '',
+                    'password_err' => ''
+                ];
+            }
+
+            // Load view
+            $this->view('students/student_login', $data);
+        }
+
+        public function createUserSession($user) {
+            // taken from the database
+            $_SESSION['user_id'] = $user->stu_id;
+            $_SESSION['user_email'] = $user->email;
+            $_SESSION['user_name'] = $user->name;
+
+            redirect('students_beginner_dashboard/index');
+        }
+
+        public function logout() {
+            unset($_SESSION['user_id']);
+            unset($_SESSION['user_email']);
+            unset($_SESSION['user_name']);
+            session_destroy();
+
+            redirect('students/login');
+        }
+
+        public function isLoggedIn() {
+            if(isset($_SESSION['user_id'])) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+?>
