84e03c05a18a2c738fb85941514e8f520aabcb17
GroupProject2021 <86022597+GroupProject2021@users.noreply.github.com>
2021-09-03 16:16:35 +0530

    Update login.php

--- app/views/admin/login.php
+++ app/views/admin/login.php
@@ -6,7 +6,7 @@
     </head>
     <body>
         <!-- TOP NAVIGATION BAR -->
-        <?php require APPROOT.'/views/inc/components/topnav.php'?>
+        <?php require APPROOT.'/views/inc/components/admin_login_navbar.php'?>
         
         <!-- LOGIN FORM -->
         <div class="form-container">
@@ -18,14 +18,14 @@
                 <input type="text" placeholder="Enter email" name="email" id="email" value="<?php echo $data['email']; ?>">
                 <span class="form-invalid"><?php echo $data['email_err']; ?></span><br>
                 <label for="password"><p class="form-bold">Password</p></label>
-                <input type="text" placeholder="Enter email" name="password" id="password" value="<?php echo $data['password']; ?>">
+                <input type="text" placeholder="Enter password" name="password" id="password" value="<?php echo $data['password']; ?>">
                 <span class="form-invalid"><?php echo $data['password_err']; ?></span><br>
                 <hr  class="form-hr">
                 <button type="submit" class="form-submit">Login</button>
             </form>
         </div>
-        <div class="form-container content">
-            <p>Don't have an account? <a class="form-link" href="<?php echo URLROOT; ?>/admins/register">Register</a></p>
+        
+            
         </div>
     </body>
 </html>
