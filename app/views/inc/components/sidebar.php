7adb99b484a1d353d7ca97ce0f4ada1e929f310c
Dhanushka sandakelum <dhanushkasandakelum711@gmail.com>
2021-09-11 19:26:07 +0530

    student sidebar updated

--- app/views/inc/components/sidebar.php
+++ app/views/inc/components/sidebar.php
@@ -19,65 +19,94 @@
         <?php if($_SESSION['specialized_actor_type'] == 'Beginner'): ?>
         <!-- Beginner to OL qualified upgrade -->
         <hr>
-        <div class="sidebar-user-details">
-            <div class="user-level-image">
-                <img src="<?php echo URLROOT; ?>/imgs/sidebar/2.jpg" alt="prof_pic">
+        <div class="sidebar-user-details-container">
+            <div class="sidebar-user-details">
+                <div class="user-level-image">
+                    <img src="<?php echo URLROOT; ?>/imgs/sidebar/2.jpg" alt="prof_pic">
+                </div>
+                <div class="user-level-content">
+                    <div class="user-level-name">
+                        <p>Beginner</p>
+                    </div>
+                    <div class="profile-upgrade-progress">
+                        <progress max="100" value="25"></progress>
+                    </div>
+                    <center><p>Profile complete: 25%</p></center>
+                </div>
             </div>
-            <div class="user-level-content">
-                <div class="user-level-name">
-                    <p>Beginner</p>
-                </div>
-                <div class="profile-upgrade-progress">
-
-                </div>
-                <div class="profile-upgrade-button-area">
-                    <a href="<?php echo URLROOT; ?>/Students_ProfileUpgrade/upgradeToOlQualified">
-                        <input class="profile-upgrade-button" type="button" value="UPGRADE TO OL">
-                    </a>
-                </div>
+            <div class="profile-upgrade-button-area">
+                <a href="<?php echo URLROOT; ?>/Students_ProfileUpgrade/upgradeToAlQualified">
+                    <input class="profile-upgrade-button" type="button" value="UPGRADE TO OL QUALIFIED">
+                </a>
             </div>
         </div>
         <hr>
         <?php elseif($_SESSION['specialized_actor_type'] == 'OL qualified'): ?>
         <!-- OL qualified to AL qualified upgrade -->
-        <hr>
-        <div class="sidebar-user-details">
-            <div class="user-level-image">
-                <img src="<?php echo URLROOT; ?>/imgs/sidebar/2.jpg" alt="prof_pic">
+        <hr>        
+        <div class="sidebar-user-details-container">
+            <div class="sidebar-user-details">
+                <div class="user-level-image">
+                    <img src="<?php echo URLROOT; ?>/imgs/sidebar/2.jpg" alt="prof_pic">
+                </div>
+                <div class="user-level-content">
+                    <div class="user-level-name">
+                        <p>OL Qualified</p>
+                    </div>
+                    <div class="profile-upgrade-progress">
+                        <progress max="100" value="50"></progress>
+                    </div>
+                    <center><p>Profile complete: 50%</p></center>
+                </div>
             </div>
-            <div class="user-level-content">
-                <div class="user-level-name">
-                    <p>OL Qualified</p>
-                </div>
-                <div class="profile-upgrade-progress">
-
-                </div>
-                <div class="profile-upgrade-button-area">
-                    <a href="<?php echo URLROOT; ?>/Students_ProfileUpgrade/upgradeToAlQualified">
-                        <input class="profile-upgrade-button" type="button" value="UPGRADE TO AL">
-                    </a>
-                </div>
+            <div class="profile-upgrade-button-area">
+                <a href="<?php echo URLROOT; ?>/Students_ProfileUpgrade/upgradeToAlQualified">
+                    <input class="profile-upgrade-button" type="button" value="UPGRADE TO AL QUALIFIED">
+                </a>
             </div>
         </div>
         <hr>
         <?php elseif($_SESSION['specialized_actor_type'] == 'AL qualified'): ?>
         <!-- AL qualified to UndergraduateGraduate upgrade -->
         <hr>
-        <div class="sidebar-user-details">
-            <div class="user-level-image">
-                <img src="<?php echo URLROOT; ?>/imgs/sidebar/2.jpg" alt="prof_pic">
+        <div class="sidebar-user-details-container">
+            <div class="sidebar-user-details">
+                <div class="user-level-image">
+                    <img src="<?php echo URLROOT; ?>/imgs/sidebar/2.jpg" alt="prof_pic">
+                </div>
+                <div class="user-level-content">
+                    <div class="user-level-name">
+                        <p>AL Qualified</p>
+                    </div>
+                    <div class="profile-upgrade-progress">
+                        <progress max="100" value="75"></progress>
+                    </div>
+                    <center><p>Profile complete: 75%</p></center>
+                </div>
             </div>
-            <div class="user-level-content">
-                <div class="user-level-name">
-                    <p>AL Qualified</p>
+            <div class="profile-upgrade-button-area">
+                <a href="<?php echo URLROOT; ?>/Students_ProfileUpgrade/upgradeToAlQualified">
+                    <input class="profile-upgrade-button" type="button" value="UPGRADE TO AL UNDERGRAD/GRAD">
+                </a>
+            </div>
+        </div>
+        <hr>
+        <?php elseif($_SESSION['specialized_actor_type'] == 'Undergraduate Graduate'):?>
+        <!-- UndergraduateGraduate profile completeness -->
+        <hr>
+        <div class="sidebar-user-details-container">
+            <div class="sidebar-user-details">
+                <div class="user-level-image">
+                    <img src="<?php echo URLROOT; ?>/imgs/sidebar/2.jpg" alt="prof_pic">
                 </div>
-                <div class="profile-upgrade-progress">
-
-                </div>
-                <div class="profile-upgrade-button-area">
-                    <a href="<?php echo URLROOT; ?>/Students_ProfileUpgrade/upgradeToUndergraduateGraduate">
-                        <input class="profile-upgrade-button" type="button" value="UPGRADE TO UNI">
-                    </a>
+                <div class="user-level-content">
+                    <div class="user-level-name">
+                        <p>Undergrad / Grad</p>
+                    </div>
+                    <div class="profile-upgrade-progress">
+                        <progress max="100" value="100"></progress>
+                    </div>
+                    <center><p>Profile complete: 100%</p></center>
                 </div>
             </div>
         </div>
