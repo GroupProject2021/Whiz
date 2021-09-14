9c50a0fd255c33f5405ff1c8bc6f99ba85331a05
Dhanushka sandakelum <dhanushkasandakelum711@gmail.com>
2021-09-14 16:24:59 +0530

    login passowrd validation features improved

--- app/views/common/user_login.php
+++ app/views/common/user_login.php
@@ -19,8 +19,23 @@
                 <label for="email"><p class="form-bold">Email</p></label>
                 <input type="text" placeholder="Enter email" name="email" id="email" value="<?php echo $data['email']; ?>">
                 <span class="form-invalid"><?php echo $data['email_err']; ?></span><br>
+                
+                
                 <label for="password"><p class="form-bold">Password</p></label>
-                <input type="password" placeholder="Enter password" name="password" id="password" value="<?php echo $data['password']; ?>">
+                <table class="form-table">
+                    <tr>
+                        <td>
+                            <input type="password" placeholder="Enter password" name="password" id="password" value="<?php echo $data['password']; ?>">
+                        </td>
+                        <td>
+                            <div class="toggle-password">
+                                <img src="<?php echo URLROOT; ?>/imgs/form/hide-eye-icon.png" class="hide-password-eye" width="25px" height="20px" alt="hide">
+                                <img src="<?php echo URLROOT; ?>/imgs/form/show-eye-icon.png" class="show-password-eye" width="25px" height="20px" alt="show">
+                            </div>
+                        </td>                    
+                    </tr>
+                </table>
+
                 <span class="form-invalid"><?php echo $data['password_err']; ?></span><br>
                 <hr  class="form-hr">
                 <button type="submit" class="form-submit">Login</button>
@@ -29,5 +44,8 @@
         <div class="form-container content">
             <p>Don't have an account? <a class="form-link" href="<?php echo URLROOT; ?>/Commons/registerRedirect">Register</a></p>
         </div>
+
+        <!-- java script form validation -->
+        <script type="text/JavaScript" src="<?php echo URLROOT; ?>/js/components/form.js"></script>
     </body>
 </html>
--- app/views/students/student_login.php
+++ app/views/students/student_login.php
@@ -19,8 +19,22 @@
                 <label for="email"><p class="form-bold">Email</p></label>
                 <input type="text" placeholder="Enter email" name="email" id="email" value="<?php echo $data['email']; ?>">
                 <span class="form-invalid"><?php echo $data['email_err']; ?></span><br>
+
                 <label for="password"><p class="form-bold">Password</p></label>
-                <input type="text" placeholder="Enter email" name="password" id="password" value="<?php echo $data['password']; ?>">
+                <table class="form-table">
+                    <tr>
+                        <td>
+                            <input type="password" placeholder="Enter password" name="password" id="password" value="<?php echo $data['password']; ?>">
+                        </td>
+                        <td>
+                            <div class="toggle-password">
+                                <img src="<?php echo URLROOT; ?>/imgs/form/hide-eye-icon.png" class="hide-password-eye" width="25px" height="20px" alt="hide">
+                                <img src="<?php echo URLROOT; ?>/imgs/form/show-eye-icon.png" class="show-password-eye" width="25px" height="20px" alt="show">
+                            </div>
+                        </td>                    
+                    </tr>
+                </table>
+
                 <span class="form-invalid"><?php echo $data['password_err']; ?></span><br>
                 <hr  class="form-hr">
                 <button type="submit" class="form-submit">Login</button>
