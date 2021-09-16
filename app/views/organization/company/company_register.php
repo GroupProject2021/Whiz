445e152b082980893aed52fdf394a484f191b887
Dhanushka sandakelum <dhanushkasandakelum711@gmail.com>
2021-09-16 17:07:50 +0530

    organization form styles resolved

--- app/views/organization/company/company_register.php
+++ app/views/organization/company/company_register.php
@@ -14,62 +14,130 @@
                 <h1>Company Register</h1>
                 <p>Please fill in this form to create an account</p>
                 <hr class="form-hr">
-                <label for="comname"><p class="form-bold">Company Name</p></label>
-                <input type="text" placeholder="Enter university name" name="comname" id="comname" value="<?php echo $data['comname']; ?>">
+
+                <!-- company name -->
                 <span class="form-invalid"><?php echo $data['comname_err']; ?></span><br>
+                <input type="text" placeholder=" " name="comname" id="comname" value="<?php echo $data['comname']; ?>">
+                <label>Company Name</label>
 
-                <label for="address"><p class="form-bold">Company Address</p></label>
-                <input type="text" placeholder="Enter address" name="address" id="address" value="<?php echo $data['address']; ?>">
+                <!-- company address -->
                 <span class="form-invalid"><?php echo $data['address_err']; ?></span><br>
+                <input type="text" placeholder=" " name="address" id="address" value="<?php echo $data['address']; ?>">
+                <label>Company address</label>
 
-                <label for="email"><p class="form-bold">Company Email</p></label>
-                <input type="text" placeholder="Enter email" name="email" id="email" value="<?php echo $data['email']; ?>">
+                <!-- company email -->
                 <span class="form-invalid"><?php echo $data['email_err']; ?></span><br>
+                <input type="text" placeholder=" " name="email" id="email" value="<?php echo $data['email']; ?>">
+                <label>Company email</label>
 
-                <label for="password"><p class="form-bold">Password</p></label>
-                <input type="password" placeholder="Enter password" name="password" id="password" value="<?php echo $data['password']; ?>">
+                <!-- password -->
                 <span class="form-invalid"><?php echo $data['password_err']; ?></span><br>
+                <div class="password-area">
+                    <div class="pasword-content">                    
+                        <input type="password" placeholder=" " name="password" id="password" value="<?php echo $data['password']; ?>">                        
+                        <label>Password</label>
+                    </div>
+                    <div class="toggle-password">
+                        <img src="<?php echo URLROOT; ?>/imgs/form/hide-eye-icon.png" class="hide-password-eye" width="25px" height="20px" alt="hide">
+                        <img src="<?php echo URLROOT; ?>/imgs/form/show-eye-icon.png" class="show-password-eye" width="25px" height="20px" alt="show">
+                    </div>
+                </div>
+                <div class="bottom-content">
+                    <div class="form-validation">
+                        <div class="policy-length">
+                            <img src="<?php echo URLROOT; ?>/imgs/form/green-tick-icon.png" width="15px" height="15px" alt="green-tick">
+                            Minimum 8 Characters
+                        </div>
+                        <div class="policy-number">
+                            <img src="<?php echo URLROOT; ?>/imgs/form/green-tick-icon.png" width="15px" height="15px" alt="green-tick">
+                            Contains a number
+                        </div>
+                        <div class="policy-uppercase">
+                            <img src="<?php echo URLROOT; ?>/imgs/form/green-tick-icon.png" width="15px" height="15px" alt="green-tick">
+                            Contains uppercase
+                        </div>
+                        <div class="policy-special">
+                            <img src="<?php echo URLROOT; ?>/imgs/form/green-tick-icon.png" width="15px" height="15px" alt="green-tick">
+                            Contains special characters
+                        </div>
+                    </div>
+                </div>
 
-                <label for="confirm_password"><p class="form-bold">Confirm password</p></label>
-                <input type="password" placeholder="Enter confirm password" name="confirm_password" id="confirm_password" value="<?php echo $data['confirm_password']; ?>">
+                <!-- confirm password -->
                 <span class="form-invalid"><?php echo $data['confirm_password_err']; ?></span><br>
+                <div class="password-area">
+                    <div class="pasword-content">                    
+                        <input type="password" placeholder=" " name="confirm_password" id="confirm_password" value="<?php echo $data['confirm_password']; ?>">                        
+                        <label>Confirm Password</label>
+                    </div>
+                    <div class="toggle-confirm-password">
+                        <img src="<?php echo URLROOT; ?>/imgs/form/hide-eye-icon.png" class="hide-password-eye" width="25px" height="20px" alt="hide">
+                        <img src="<?php echo URLROOT; ?>/imgs/form/show-eye-icon.png" class="show-password-eye" width="25px" height="20px" alt="show">
+                    </div>
+                </div>       
+                <div class="bottom-content">
+                    <div class="form-validation">
+                        <div class="policy-password-match">
+                            <img src="<?php echo URLROOT; ?>/imgs/form/green-tick-icon.png" width="15px" height="15px" alt="green-tick">
+                            Passwords are matching
+                        </div>
+                    </div>
+                </div>
 
-                <label for="phn_no"><p class="form-bold">Phone number</p></label>
-                <input type="text" placeholder="Enter phone number" name="phn_no" id="phn_no" value="<?php echo $data['phn_no']; ?>">
+
+                <!-- phone number -->
                 <span class="form-invalid"><?php echo $data['phn_no_err']; ?></span><br>
+                <input type="text" placeholder=" " name="phn_no" id="phn_no" value="<?php echo $data['phn_no']; ?>">
+                <label>Phone number</label>
+                <div class="bottom-content">
+                    <div class="form-validation">
+                        <div class="phn_no-validation">
+                            <img src="<?php echo URLROOT; ?>/imgs/form/green-tick-icon.png" width="15px" height="15px" alt="green-tick">
+                            Your phone number is valid
+                        </div>
+                    </div>
+                </div>
 
-                <label for="website"><p class="form-bold">Company Web Address</Address></p></label>
-                <input type="text" placeholder="Enter web address" name="website" id="website" value="<?php echo $data['website']; ?>">
+                <!-- company web address -->
                 <span class="form-invalid"><?php echo $data['website_err']; ?></span><br>
+                <input type="text" placeholder=" " name="website" id="website" value="<?php echo $data['website']; ?>">
+                <label>Company web address</label>
 
-                <label for="founder"><p class="form-bold">Founder</p></label>
-                <input type="text" placeholder="Enter founder name" name="founder" id="founder" value="<?php echo $data['founder']; ?>">
+                <!-- founder -->
                 <span class="form-invalid"><?php echo $data['founder_err']; ?></span><br>
+                <input type="text" placeholder=" " name="founder" id="founder" value="<?php echo $data['founder']; ?>">
+                <label>Founder</label>
 
-                <label for="founded_year"><p class="form-bold">Founded Year</p></label>
-                <input type="text" placeholder="Enter founder name" name="founded_year" id="founded_year" value="<?php echo $data['founded_year']; ?>">
+                <!-- founded year -->
                 <span class="form-invalid"><?php echo $data['founded_year_err']; ?></span><br>
+                <input type="text" placeholder=" " name="founded_year" id="founded_year" value="<?php echo $data['founded_year']; ?>">
+                <label>Founded Year</label>
 
-                <label for="cur_emp"><p class="form-bold">No. of Current employees</p></label>
-                <input type="text" placeholder="Enter no. of employees" name="cur_emp" id="cur_emp" value="<?php echo $data['cur_emp']; ?>">
+                <!-- no. of current employees -->
                 <span class="form-invalid"><?php echo $data['cur_emp_err']; ?></span><br>
+                <input type="text" placeholder=" " name="cur_emp" id="cur_emp" value="<?php echo $data['cur_emp']; ?>">
+                <label>No. of Current Employees</label>
 
-                <label for="emp_size"><p class="form-bold">Company Size</p></label>
-                <input type="text" placeholder="Enter capable no. of employees" name="emp_size" id="emp_size" value="<?php echo $data['emp_size']; ?>">
+                <!-- company size -->
                 <span class="form-invalid"><?php echo $data['emp_size_err']; ?></span><br>
+                <input type="text" placeholder=" " name="emp_size" id="emp_size" value="<?php echo $data['emp_size']; ?>">
+                <label>Comapny Size</label>
 
-                <label for="registered"><p class="form-bold">Registered Company?</p></label><br>
+                <!-- registered company? -->
+                <br><p class="form-bold">Registered Company?</p><br>
                 <input type="radio"  name="registered" value="Yes">Yes
                 <input type="radio"  name="registered" value="No">No
                 <span class="form-invalid"><?php echo $data['registered_err']; ?></span><br>
 
-                <label for="overview"><p class="form-bold">Overview</p></label>
-                <input type="text" placeholder="Enter overview" name="overview" id="overview" value="<?php echo $data['overview']; ?>">
+                <!-- overview -->
                 <span class="form-invalid"><?php echo $data['overview_err']; ?></span><br>
+                <input type="text" placeholder=" " name="overview" id="overview" value="<?php echo $data['overview']; ?>">
+                <label>Overview</label>
 
-                <label for="services"><p class="form-bold">Services</p></label>
-                <input type="text" placeholder="Enter specialities of company" name="services" id="services" value="<?php echo $data['services']; ?>">
+                <!-- services -->
                 <span class="form-invalid"><?php echo $data['services_err']; ?></span><br>
+                <input type="text" placeholder=" " name="services" id="services" value="<?php echo $data['services']; ?>">
+                <label>Services</label>
 
                 <hr class="form-hr">
                 <p>By creating an account you agree to our <a class="form-link" href="#">Terms & Privacy</a></p>
@@ -79,5 +147,9 @@
         <div class="form-container content">
             <p>Already have an account? <a class="form-link" href="<?php echo URLROOT; ?>/commons/login">Sign in</a></p>
         </div>
+
+        <!-- java script form validation -->
+        <script type="text/JavaScript" src="<?php echo URLROOT; ?>/js/components/form.js"></script>
+        <script type="text/JavaScript" src="<?php echo URLROOT; ?>/js/components/imageupload.js"></script>
     </body>
 </html>
--- app/views/organization/university/university_register.php
+++ app/views/organization/university/university_register.php
@@ -14,64 +14,132 @@
                 <h1>University Register</h1>
                 <p>Please fill in this form to create an account</p>
                 <hr class="form-hr">
-                <label for="uniname"><p class="form-bold">University Name</p></label>
-                <input type="text" placeholder="Enter university name" name="uniname" id="uniname" value="<?php echo $data['uniname']; ?>">
+
+                <!-- university name -->
                 <span class="form-invalid"><?php echo $data['uniname_err']; ?></span><br>
+                <input type="text" placeholder=" " name="uniname" id="uniname" value="<?php echo $data['uniname']; ?>">
+                <label>University Name</label>
 
-                <label for="address"><p class="form-bold">University Address</p></label>
-                <input type="text" placeholder="Enter address" name="address" id="address" value="<?php echo $data['address']; ?>">
+                <!-- address -->
                 <span class="form-invalid"><?php echo $data['address_err']; ?></span><br>
+                <input type="text" placeholder=" " name="address" id="address" value="<?php echo $data['address']; ?>">
+                <label>Address</label>
 
-                <label for="email"><p class="form-bold">University Email</p></label>
-                <input type="text" placeholder="Enter email" name="email" id="email" value="<?php echo $data['email']; ?>">
+                <!-- email -->
                 <span class="form-invalid"><?php echo $data['email_err']; ?></span><br>
+                <input type="text" placeholder=" " name="email" id="email" value="<?php echo $data['email']; ?>">
+                <label>Email</label>
 
-                <label for="password"><p class="form-bold">Password</p></label>
-                <input type="password" placeholder="Enter password" name="password" id="password" value="<?php echo $data['password']; ?>">
+                <!-- password -->
                 <span class="form-invalid"><?php echo $data['password_err']; ?></span><br>
+                <div class="password-area">
+                    <div class="pasword-content">                    
+                        <input type="password" placeholder=" " name="password" id="password" value="<?php echo $data['password']; ?>">                        
+                        <label>Password</label>
+                    </div>
+                    <div class="toggle-password">
+                        <img src="<?php echo URLROOT; ?>/imgs/form/hide-eye-icon.png" class="hide-password-eye" width="25px" height="20px" alt="hide">
+                        <img src="<?php echo URLROOT; ?>/imgs/form/show-eye-icon.png" class="show-password-eye" width="25px" height="20px" alt="show">
+                    </div>
+                </div>
+                <div class="bottom-content">
+                    <div class="form-validation">
+                        <div class="policy-length">
+                            <img src="<?php echo URLROOT; ?>/imgs/form/green-tick-icon.png" width="15px" height="15px" alt="green-tick">
+                            Minimum 8 Characters
+                        </div>
+                        <div class="policy-number">
+                            <img src="<?php echo URLROOT; ?>/imgs/form/green-tick-icon.png" width="15px" height="15px" alt="green-tick">
+                            Contains a number
+                        </div>
+                        <div class="policy-uppercase">
+                            <img src="<?php echo URLROOT; ?>/imgs/form/green-tick-icon.png" width="15px" height="15px" alt="green-tick">
+                            Contains uppercase
+                        </div>
+                        <div class="policy-special">
+                            <img src="<?php echo URLROOT; ?>/imgs/form/green-tick-icon.png" width="15px" height="15px" alt="green-tick">
+                            Contains special characters
+                        </div>
+                    </div>
+                </div>
 
-                <label for="confirm_password"><p class="form-bold">Confirm password</p></label>
-                <input type="password" placeholder="Enter confirm password" name="confirm_password" id="confirm_password" value="<?php echo $data['confirm_password']; ?>">
+                <!-- confirm password -->
                 <span class="form-invalid"><?php echo $data['confirm_password_err']; ?></span><br>
+                <div class="password-area">
+                    <div class="pasword-content">                    
+                        <input type="password" placeholder=" " name="confirm_password" id="confirm_password" value="<?php echo $data['confirm_password']; ?>">                        
+                        <label>Confirm Password</label>
+                    </div>
+                    <div class="toggle-confirm-password">
+                        <img src="<?php echo URLROOT; ?>/imgs/form/hide-eye-icon.png" class="hide-password-eye" width="25px" height="20px" alt="hide">
+                        <img src="<?php echo URLROOT; ?>/imgs/form/show-eye-icon.png" class="show-password-eye" width="25px" height="20px" alt="show">
+                    </div>
+                </div>       
+                <div class="bottom-content">
+                    <div class="form-validation">
+                        <div class="policy-password-match">
+                            <img src="<?php echo URLROOT; ?>/imgs/form/green-tick-icon.png" width="15px" height="15px" alt="green-tick">
+                            Passwords are matching
+                        </div>
+                    </div>
+                </div>
 
-                <label for="phn_no"><p class="form-bold">Phone number</p></label>
-                <input type="text" placeholder="Enter phone number" name="phn_no" id="phn_no" value="<?php echo $data['phn_no']; ?>">
+                <!-- phone number -->
                 <span class="form-invalid"><?php echo $data['phn_no_err']; ?></span><br>
+                <input type="text" placeholder=" " name="phn_no" id="phn_no" value="<?php echo $data['phn_no']; ?>">
+                <label>Phone number</label>
+                <div class="bottom-content">
+                    <div class="form-validation">
+                        <div class="phn_no-validation">
+                            <img src="<?php echo URLROOT; ?>/imgs/form/green-tick-icon.png" width="15px" height="15px" alt="green-tick">
+                            Your phone number is valid
+                        </div>
+                    </div>
+                </div>
 
-                <label for="website"><p class="form-bold">University Web Address</Address></p></label>
-                <input type="text" placeholder="Enter web address" name="website" id="website" value="<?php echo $data['website']; ?>">
+                <!-- website -->
                 <span class="form-invalid"><?php echo $data['website_err']; ?></span><br>
+                <input type="text" placeholder=" " name="website" id="website" value="<?php echo $data['website']; ?>">
+                <label>Website</label>
 
-                <label for="founder"><p class="form-bold">Founder</p></label>
-                <input type="text" placeholder="Enter founder name" name="founder" id="founder" value="<?php echo $data['founder']; ?>">
+                <!-- founder -->
                 <span class="form-invalid"><?php echo $data['founder_err']; ?></span><br>
+                <input type="text" placeholder=" " name="founder" id="founder" value="<?php echo $data['founder']; ?>">
+                <label>Founder</label>
 
-                <label for="founded_year"><p class="form-bold">Founded Year</p></label>
-                <input type="text" placeholder="Enter founder name" name="founded_year" id="founded_year" value="<?php echo $data['founded_year']; ?>">
+                <!-- founded year -->
                 <span class="form-invalid"><?php echo $data['founded_year_err']; ?></span><br>
+                <input type="text" placeholder=" " name="founded_year" id="founded_year" value="<?php echo $data['founded_year']; ?>">
+                <label>Founded Year</label>
 
-                <label for="approved"><p class="form-bold">UGC Approved?</p></label><br>
+                <!-- ugc approved? -->
+                <br><p class="form-bold">UGC Approved?</p><br>
                 <input type="radio"  name="approved" value="Yes">Yes
                 <input type="radio"  name="approved" value="No">No
                 <span class="form-invalid"><?php echo $data['approved_err']; ?></span><br>
 
-                <label for="rank"><p class="form-bold">World Rank</p></label>
-                <input type="text" placeholder="Enter world rank" name="rank" id="rank" value="<?php echo $data['rank']; ?>">
+                <!-- world rank -->
                 <span class="form-invalid"><?php echo $data['rank_err']; ?></span><br>
+                <input type="text" placeholder=" " name="rank" id="rank" value="<?php echo $data['rank']; ?>">
+                <label>World Rank</label>
 
-                <label for="amount"><p class="form-bold">Student Amount</p></label>
-                <input type="text" placeholder="Enter capable no. of students" name="amount" id="amount" value="<?php echo $data['amount']; ?>">
+                <!-- student amount -->
                 <span class="form-invalid"><?php echo $data['amount_err']; ?></span><br>
+                <input type="text" placeholder=" " name="amount" id="amount" value="<?php echo $data['amount']; ?>">
+                <label>Student Amount</label>
 
-                <label for="rate"><p class="form-bold">Graduate Job Rate</p></label>
-                <input type="text" placeholder="Enter job rate" name="rate" id="rate" value="<?php echo $data['rate']; ?>">
+                <!-- graduate job rate -->
                 <span class="form-invalid"><?php echo $data['rate_err']; ?></span><br>
+                <input type="text" placeholder=" " name="rate" id="rate" value="<?php echo $data['rate']; ?>">
+                <label>Graduate Job Rate</label>
 
-                <label for="descrip"><p class="form-bold">Description</p></label>
-                <input type="text" placeholder="Enter description" name="descrip" id="descrip" value="<?php echo $data['descrip']; ?>">
+                <!-- description -->
                 <span class="form-invalid"><?php echo $data['descrip_err']; ?></span><br>
+                <input type="text" placeholder=" " name="descrip" id="descrip" value="<?php echo $data['descrip']; ?>">
+                <label>Desciption</label>
 
-                <label for="type"><p class="form-bold">University Type</p></label><br>
+                <!-- university type -->
+                <br><p class="form-bold">University Type</p><br>
                 <input type="radio" name="type" value="Semi Government">Semi-Government(Government Affiliated)<br>
                 <input type="radio" name="type" value="Private">Private
                 <span class="form-invalid"><?php echo $data['type_err']; ?></span><br>
@@ -84,5 +152,9 @@
         <div class="form-container content">
             <p>Already have an account? <a class="form-link" href="<?php echo URLROOT; ?>/commons/login">Sign in</a></p>
         </div>
+
+        <!-- java script form validation -->
+        <script type="text/JavaScript" src="<?php echo URLROOT; ?>/js/components/form.js"></script>
+        <script type="text/JavaScript" src="<?php echo URLROOT; ?>/js/components/imageupload.js"></script>
     </body>
 </html>
