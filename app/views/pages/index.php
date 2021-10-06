72d26e595830078750dd455637bf21823cf95991
Pabasara <pabasaraasvini12@gmail.com>
2021-10-06 14:04:50 +0530

    whiz home page contents

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -15,6 +15,33 @@
                     eos obcaecati nemo. Provident voluptatem modi rerum. <br>
                 </p>
             </center>
+            <p>Whiz, Guider of your future<br> Whiz is a platform that collaborate students, universities, companies, teachers 
+            and professional guiders through our system.</p>
+
+            <h1>Students</h1>
+            <p>Whiz is for all the students.
+            <br>You just have to register on Whiz, and we will help you to find the most 
+            <br>suitable streams, courses based on your performance.</p>
+            <h1>Undergraduates / Graduates</h1>
+            <p>Whiz is team up with many organizations and companies in the industry.<br>
+            We publish hundreds of job oppertunities a day. You can apply for them here.<br></p>
+
+            <h1>Companies</h1>
+            <p>Whiz can help you to fill institutional vacancies of your company. <br>
+            All you have to do is register on Whiz as a Company under Organization category<br>
+            and post your job notices. </p>
+
+            <h1>Universities</h1>
+            <p>Private universities and Semi-government universities can register under Organization category<br>
+            and post about intake notices and course offerings through our Whiz platform.</p>
+
+            <h1>Professional Guider</h1>
+            <p>If you are an organization which offers globally recognized career qualifications,<br>
+            Whiz is a great oppertunity for you to educate people. By registering as a professional guider<br>
+            under mentor category, you are permitted to post about career sessions to your enthusiast.</p>
+            <h1>Teacher</h1>
+            <p>If you are willing to help to the Advanced Level qualified students on getting basic<br>
+            knowledge about their selected courses, Whiz is the best platform. </p>
         </div>
         <div class="home-grid-footer">
             <?php require APPROOT.'/views/inc/components/bottomnav.php'?>
