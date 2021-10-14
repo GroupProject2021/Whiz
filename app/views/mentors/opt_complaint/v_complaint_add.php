de98366d2ec9837da49330a735fdbad8b6a1116c
Pabasara <pabasaraasvini12@gmail.com>
2021-10-15 00:19:53 +0530

    minor changes

--- app/views/mentors/opt_complaint/v_complaint_add.php
+++ app/views/mentors/opt_complaint/v_complaint_add.php
@@ -29,14 +29,10 @@
                     
                         <form action="<?php echo URLROOT; ?>/C_M_Complaints/add" method="post" enctype="multipart/form-data">
                             <div class="post-creator">
-                                <!-- <div class="post-creator-image">
-                                    <img src="" alt="" id="image_placeholder" style="display: none;">
-                                </div> -->
+                                
                                 <div class="post-creator-title">
                                     <input type="text" name="title" id="title" autocomplete="off" placeholder="Title" value="<?php echo $data['title']; ?>">
-                                    <!-- <div class="image-select"><img src="<?php echo URLROOT.'/imgs/components/posts/add-image-icon.png'; ?>" alt="" id="addImageBtn" onclick="toggleBrowse()"></div> -->
-                                    <!-- <div class="image-select"><img src="<?php echo URLROOT.'/imgs/components/posts/remove-image-icon.png'; ?>" alt="" id="removeImageBtn" onclick="removeImage()" style="display: none;"></div> -->
-                                    <!-- <input type="file" name="image" id="image" onchange="displayImage(this)" style="display: none;"> -->
+                                    
                                 </div>
                                 <hr>
                                 <div class="post-creator-content">
@@ -57,59 +53,5 @@
                 </div>
             </main>
         </div>
-        <!-- <script>
-            const addImageBtn = document.getElementById("addImageBtn");
-            const removeImageBtn = document.getElementById("removeImageBtn");
-            const imageplaceholder = document.getElementById("image_placeholder");
-
-            let inputPath = document.querySelector("#image");
-
-            let file;
-
-            function toggleBrowse() {
-                inputPath.click();
-            }
-
-            function removeImage() {
-                addImageBtn.style.display = "block";
-                removeImageBtn.style.display = "none";
-                imageplaceholder.style.display = "none";
-
-                imageplaceholder.setAttribute('src', '');
-
-                inputPath.value = null;
-            }
-
-            inputPath.addEventListener("change", function() {
-                file = this.files[0];
-
-                addImageBtn.style.display = "none";
-                removeImageBtn.style.display = "block";
-                imageplaceholder.style.display = "block";
-
-                showImage();    
-            });
-            
-            function showImage() {
-                let fileType = file.type;
-
-                let validExtensions = ["image/jpeg", "image/jpg", "image/png"];
-
-                if(validExtensions.includes(fileType)) {
-                    let fileReader = new FileReader();
-
-                    fileReader.onload = ()=>{
-                        let fileURL = fileReader.result;
-
-                        imageplaceholder.setAttribute('src', fileURL);
-                    }
-
-                    fileReader.readAsDataURL(file);
-
-                }
-                else {
-                    alert("This is not an Image file");
-                }
-            }
-        </script>
+        
 <?php require APPROOT.'/views/inc/footer.php'; ?> -->
--- app/views/mentors/opt_complaint/v_complaint_edit.php
+++ app/views/mentors/opt_complaint/v_complaint_edit.php
@@ -29,14 +29,10 @@
                     
                         <form action="<?php echo URLROOT; ?>/C_M_Complaints/edit/<?php echo $data['id']; ?>" method="post" enctype="multipart/form-data">
                             <div class="post-creator">
-                                <!-- <div class="post-creator-image" id="post-creator-image">
-                                    <img src="<?php if($data['image_name'] != null){ echo URLROOT.'/imgs/posts/banners/'.$data['image_name'];}else{ echo '';} ?>" alt="" id="image_placeholder" style="display: none;">
-                                </div> -->
+                                
                                 <div class="post-creator-title">
                                     <input type="text" name="title" id="title" autocomplete="off" placeholder="Title" value="<?php echo $data['title']; ?>">
-                                    <!-- <div class="image-select"><img src="<?php echo URLROOT.'/imgs/components/posts/add-image-icon.png'; ?>" alt="" id="addImageBtn" onclick="toggleBrowse()"></div> -->
-                                    <!-- <div class="image-select"><img src="<?php echo URLROOT.'/imgs/components/posts/remove-image-icon.png'; ?>" alt="" id="removeImageBtn" onclick="removeImage()" style="display: none;"></div> -->
-                                    <!-- <input type="file" name="image" id="image" onchange="displayImage(this)" style="display: none;"> -->
+                                    
                                 </div>
                                 <hr>
                                 <div class="post-creator-content">
@@ -55,66 +51,5 @@
                 </div>
             </main>
         </div>
-        <!-- <script>
-            const addImageBtn = document.getElementById("addImageBtn");
-            const removeImageBtn = document.getElementById("removeImageBtn");
-            const imageplaceholder = document.getElementById("image_placeholder");
-
-            let inputPath = document.querySelector("#image");
-
-            let file;
-
-            // if image exists
-            if(imageplaceholder.getAttribute('src') != ''){
-                addImageBtn.style.display = "none";
-                removeImageBtn.style.display = "block";
-                imageplaceholder.style.display = "block";
-            }
-
-            function toggleBrowse() {
-                inputPath.click();
-            }
-
-            function removeImage() {
-                addImageBtn.style.display = "block";
-                removeImageBtn.style.display = "none";
-                imageplaceholder.style.display = "none";
-
-                imageplaceholder.setAttribute('src', '');
-
-                inputPath.value = null;
-            }
-
-            inputPath.addEventListener("change", function() {
-                file = this.files[0];
-
-                addImageBtn.style.display = "none";
-                removeImageBtn.style.display = "block";
-                imageplaceholder.style.display = "block";
-
-                showImage();    
-            });
-            
-            function showImage() {
-                let fileType = file.type;
-
-                let validExtensions = ["image/jpeg", "image/jpg", "image/png"];
-
-                if(validExtensions.includes(fileType)) {
-                    let fileReader = new FileReader();
-
-                    fileReader.onload = ()=>{
-                        let fileURL = fileReader.result;
-
-                        imageplaceholder.setAttribute('src', fileURL);
-                    }
-
-                    fileReader.readAsDataURL(file);
-
-                }
-                else {
-                    alert("This is not an Image file");
-                }
-            }
-        </script>
+        
 <?php require APPROOT.'/views/inc/footer.php'; ?> -->
