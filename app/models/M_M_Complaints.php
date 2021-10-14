d38fb233dfba68a30b6d0437deee557b25c7e444
Pabasara <pabasaraasvini12@gmail.com>
2021-10-14 23:57:32 +0530

    mentor commplaint added-delete option not working

---
+++ app/models/M_M_Complaints.php
@@ -0,0 +1,82 @@
+<?php 
+    class M_M_Complaints{
+        private $db;
+
+        public function __construct() {
+            $this->db = new Database;
+        }
+
+        public function getComplaintById($id) {
+            $this->db->query('SELECT * FROM complaints WHERE id = :id');
+            $this->db->bind(':id', $id);
+
+            $row = $this->db->single();
+
+            return $row;
+        }
+
+        public function getComplaints() {
+            $this->db->query("SELECT *, 
+                                complaints.id AS postId,
+                                users.id AS userId,
+                                complaints.created_at as postCreated
+                                FROM complaints
+                                INNER JOIN users  
+                                ON complaints.user_id = users.id 
+                                ORDER BY complaints.created_at DESC");
+            $results = $this->db->resultSet();
+
+            return $results;
+        }
+
+        public function add($data) {
+            $this->db->query('INSERT INTO complaints(user_id, title, content) VALUES(:user_id, :title, :content)');
+            // bind values
+            // $this->db->bind(":image", $data['image_name']);
+            $this->db->bind(":user_id", $data['user_id']);
+            $this->db->bind(":title", $data['title']);
+            $this->db->bind(":content", $data['content']);
+            
+            // Execute
+            if($this->db->execute()) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        public function update($data) {
+            $this->db->query('UPDATE complaints SET title = :title, content = :content WHERE id = :id');
+            // bind values
+            
+            // $this->db->bind(":image", $data['image_name']);            
+            $this->db->bind(":id", $data['id']);
+            $this->db->bind(":title", $data['title']);
+            $this->db->bind(":content", $data['content']);
+
+            // Execute
+            if($this->db->execute()) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        public function delete($id) {
+            $this->db->query('DELETE FROM complaints WHERE id = :id');
+            // bind values
+            
+            $this->db->bind(":id", $id);
+
+            // Execute
+            if($this->db->execute()) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+?>
--- app/views/mentors/opt_complaint/v_complaint_index.php
+++ app/views/mentors/opt_complaint/v_complaint_index.php
@@ -25,26 +25,45 @@
                         </div>
 
                         <!-- MIDDLE PANEL -->
-                        <div class="middle-panel">
+                        <div class="middle-panel-single">
                             <?php flash('post_message'); ?>
-                                <a href="<?php echo URLROOT; ?>/Mentors_dashboard/add">
-                                    <button class="btn1">New Complaint</button>
-                                </a>
+                            <!-- <br> -->
+                                <a href="<?php echo URLROOT; ?>/C_M_Complaints/add"><button class="btn1">New Complaint</button> </a>
                                 <br>
-                                <div class="banner">
+
+                                <div>
+                                    <table class="gov-course-table">
+                                    <tr>
+                                        <th>    </th>
+                                        <th>Complaint Title</th>
+                                        <th colspan="2">Description</th>
+                                        <th colspan="2">Date</th>
+                                        <th> </th>
+                                        <th> </th>
+                                    </tr>
+                                    <tr><td colspan="3"><hr></td></tr>
+                            
                                     <?php foreach($data['posts'] as $post): ?>
-                                        <!-- I added this later. So now it will only show the posts that are related to the user. Remove if statement and it will show all the posts -->
-                                        <?php if($post->id == $_SESSION['user_id']): ?>
-                                            <div>                                
-                                                <h1><?php echo $post->title; ?></h1>
-                                                <br>
-                                                <p>Written by index <?php echo $post->id; ?> which is <?php echo $post->name; ?></p>
-                                                on <?php echo $post->postCreated; ?>
-                                                <p><?php echo $post->body; ?></p>
-                                                <a href="<?php echo URLROOT; ?>/Mentors_dashboard/show/<?php echo $post->postId; ?>"><button class="btn9">View More...</button></a>
-                                            </div>
+                                        <?php if($post->userId == $_SESSION['user_id']): ?>
+                                            <tr>
+                                                <!-- <td class="gov-course-index"><?php echo $post->id; ?></td> -->
+                                                <td>    </td>
+                                                <td class="gov-course-name" ><?php echo $post->title; ?></td>
+                                                <td class="gov-course-name" colspan="2"><?php echo $post->content; ?></td>
+                                                <td class="gov-course-name"><?php echo $post->created_at; ?></td>
+                                                <!-- <td class="gov-course-uniicon"><img src="<?php echo URLROOT.'/imgs/prof.jpg'?>" alt=""></td> -->
+                                                <!-- <td class="gov-course-uniname">UCSC</td> -->
+                                                <!-- <td class="gov-course-duration">4 Years</td> -->
+                                                <!-- <td class="gov-course-intake">200</td> -->
+                                                <td class="gov-course-viewmore"><a href="<?php echo URLROOT.'/C_M_Complaints/edit/'.$post->postId;?>"><button class="btn5">Edit</button></a></td>
+                                                <td class="gov-course-viewmore"><a href="<?php echo URLROOT.'/C_M_Complaints/delete/'.$post->postId;?>"><button class="btn4">Delete</button></a></td>
+                                            </tr>
+                                            <tr><td colspan="4"><hr></td></tr>
                                         <?php endif; ?>
                                     <?php endforeach; ?>
+                            
+                                    </table>
+                                    <hr>
                                 </div>
                         </div>
                     </div>
